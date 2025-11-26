5bbc9cf1537db5b90a2ef5ed154d760888ea8acf
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2025-11-26 19:40:08 -0300

    format

--- lua/mpa/deps.lua
+++ lua/mpa/deps.lua
@@ -1,11 +1,11 @@
-vim.pack.add({ "https://github.com/nvim-mini/mini.deps" }, { confirm = false })
-require'mini.deps'.setup{}
+vim.pack.add({ 'https://github.com/nvim-mini/mini.deps' }, { confirm = false })
+require('mini.deps').setup {}
 
 -- Helper: add one or many plugin specs
 local function add_specs(specs)
   local t = type(specs)
 
-  if t == "table" then
+  if t == 'table' then
     -- List of specs: { {...}, {...}, ... }
     if specs[1] ~= nil then
       for _, spec in ipairs(specs) do
@@ -15,19 +15,17 @@ local function add_specs(specs)
       -- Single spec table: { source = "...", ... }
       MiniDeps.add(specs)
     end
-
-  elseif t == "string" then
+  elseif t == 'string' then
     -- Shorthand: "user/repo"
-    MiniDeps.add({ source = specs })
-
+    MiniDeps.add { source = specs }
   else
-    error(("Add(): invalid spec of type %s"):format(t))
+    error(('Add(): invalid spec of type %s'):format(t))
   end
 end
 
-Add   = add_specs
-XAdd   = function() end
-Now   = MiniDeps.now   -- keep original semantics
-XNow   = function() end
+Add = add_specs
+XAdd = function() end
+Now = MiniDeps.now -- keep original semantics
+XNow = function() end
 Later = MiniDeps.later -- keep original semantics
 XLater = function() end
